612642ddf0c13757503dd2d2e6f0250fdb772140
Dobby <wenzufan@localhost>
2019-11-04 11:21:47 +0800

    服务消费者

---
+++ src/main/java/com/dobby/dubboconsumer/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.dobby.dubboconsumer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.dobby.dubbo_interface.HelloService;
+
+@RestController
+public class HelloController {
+    @Reference
+    private HelloService helloService;
+
+    @RequestMapping("/hello")
+    public String hello() {
+        String hello = helloService.sayHello("world");
+        System.out.println(helloService.sayHello("Dobby"));
+        return hello;
+    }
+}
